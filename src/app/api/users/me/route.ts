3dc634205041b7209c5ffa592d9ecc26b57219a0
sumit sengar <87333260+sengarsumit@users.noreply.github.com>
2025-03-08 00:41:54 +0530

    middleware

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,25 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import {connect} from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request: NextRequest) {
+    try {
+         const userID=await getDataFromToken(request);
+         const user=await User.findOne(_id:userID).select('-password');
+         return NextResponse.json({
+            message: 'User fetched successfully',
+            data: user
+         })
+    } catch (error:any) {
+        return NextResponse.json({
+            status: 400,
+            statusText: error.message
+        })
+        
+    }
+}
+
